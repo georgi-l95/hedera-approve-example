2eb8a7823406644604aa8b39a62cbf14661f45ff
georgi-l95 <glazarov95@gmail.com>
2024-03-14 12:53:00 +0200

    cleanup
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,4 @@
-import {
-  Wallet,
-  LocalProvider,
-  ContractCreateTransaction,
-  ContractExecuteTransaction,
-  FileCreateTransaction,
-  ContractFunctionParameters,
-  ContractCallQuery,
-  Hbar,
-  Client,
-  PrivateKey,
-} from "@hashgraph/sdk";
+import { PrivateKey } from "@hashgraph/sdk";
 
 import { Utils } from "./utils.js";
 
@@ -29,36 +18,15 @@ async function deploy() {
     Utils.initClient()
   );
   const accountKey = PrivateKey.generateECDSA();
-  // const accountKey1 = PrivateKey.fromStringECDSA();
-  // console.log(accountKey.toStringRaw());
   const alias = accountKey.publicKey.toEvmAddress();
   const alice = await Utils.transferAccountEvm(
     wallet,
     process.env.OPERATOR_ID,
     alias
   );
-  console.log("Key to add to metamask: " + accountKey.toStringRaw());
-  // await Utils.transferAccountEvm(wallet,process.env.OPERATOR_ID, alias );
-  // await Utils.ethereumTransactionDispatch(wallet, accountKey);
-  // await Utils.ethereumTransactionDispatch(wallet, accountKey);
-  // await Utils.ethereumTransactionDispatch(wallet, accountKey);
-  // await Utils.deleteAccount(wallet, alice.accountId, accountKey);
+  await Utils.deleteAccount(wallet, alice.accountId, accountKey);
   // const bob = await Utils.transferAccountEvm(wallet,process.env.OPERATOR_ID, alias );
-  // const bob = await Utils.createAccount(wallet, "0x9326f0Aa42aBa1eD2fC3E55f59AB86D741dF5068")
-  // const contractId = await Utils.deployContract(wallet);
-  // const tokenId = await Utils.createHTSToken(wallet);
-  // const nftTokenId = await Utils.createNFTToken(wallet);
-
-  // const nftCollection = await Utils.mintNFT(wallet, nftTokenId);
-
-  // await Utils.tokenAssociate(wallet, alice.accountId, alice.accountKey, [tokenId, nftTokenId]);
-  // await Utils.tokenAssociate(wallet, contractId, null, [tokenId, nftTokenId], true);
-
-  // await Utils.transferToken(wallet, alice.accountId, tokenId, 50);
-  // await Utils.transferNFTs(wallet, wallet.accountId, alice.accountId, nftTokenId, nftCollection);
-
-  // await Utils.approveToken(wallet, alice, contractId.num, tokenId, 10);
-  // await Utils.approveNFTToken(wallet, alice, contractId.num, nftTokenId, nftCollection[0].serials[0]);
+  const bob = await Utils.createAccount(wallet, alias, accountKey);
   process.exit();
 }
 
